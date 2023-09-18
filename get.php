4e02fcec93dfe8527e83383af80545d014d40efd
dlam0006 <98052542+dlam0006@users.noreply.github.com>
2023-09-18 00:59:22 -0500

    Create get.php

---
+++ get.php
@@ -0,0 +1,12 @@
+<?php
+$pageTitle = "Get";
+include "view-header";
+?>
+<h1>Get</h1>
+<form method="get" action="get-result.php">
+  <input type="text" name="my-name">
+  <input type="submit" value="Get Data">
+</form>
+<?php
+include "view-footer";
+?>
